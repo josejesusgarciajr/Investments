49f52207fe16215087a82c78091e4c9768f0384b
josejesusgarciajr <josejesusgarciajr@gmail.com>
2022-04-16 02:42:06 -0700

    please go back

--- Investments/Startup.cs
+++ Investments/Startup.cs
@@ -29,6 +29,7 @@ namespace Investments
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
