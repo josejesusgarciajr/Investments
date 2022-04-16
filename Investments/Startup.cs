af36b26289f0d1881db8e6048961afcdd35a80c7
josejesusgarciajr <josejesusgarciajr@gmail.com>
2022-04-16 02:37:46 -0700

    delete swagger

--- Investments/Startup.cs
+++ Investments/Startup.cs
@@ -29,12 +29,7 @@ namespace Investments
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseSwagger();
-            app.UseSwagger(c =>
-            {
-                c.RouteTemplate = string.Empty;
-            });
-
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
